66ddd0ee0dc8145ffb1a30acc78b316d94e0e4cb
yska02 <yesikaa02@gmail.com>
2022-06-19 14:54:41 +0700

    snow

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -41,7 +41,7 @@ export class MainMenuComponent implements OnInit {
       this.renderer.addClass(span,'snowflake');
       this.renderer.setStyle(span,'left',emojiLeftPosition + 'px');
       this.renderer.setStyle(span,'animation-duration',(this.randomMinMax(3,5.0)+'s',this.randomMinMax(3,5.0)+'s'));
-      this.renderer.setStyle(span,'animation-delay',this.randomMinMax(0.3,2.0)+'s');
+      this.renderer.setStyle(span,'animation-delay',this.randomMinMax(0.3,30)+'s');
       this.renderer.appendChild(this.el.nativeElement,span)
     }
   }
